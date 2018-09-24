b3cb7ea3587e43d929714c1f22184c845b592bd9
Horus <horus.cristian@gmail.com>
2018-09-24 11:55:02 -0300

    Reg em ponteiro

--- tad.c
+++ tad.c
@@ -6,12 +6,12 @@ void criar(struct REGISTRO *dado){/*função usada para preencher os dados do pr
 	printf("Insira os dados..\n");
 	printf("NOME: ");
 	setbuf(stdin,NULL);
-	scanf("%[^\n]s",dado->nome);
-	setbuf(stdin,NULL);
+	scanf("%s",dado->nome);
 	printf("IDADE: ");
 	setbuf(stdin,NULL);
 	scanf("%d",&dado->idade);
 	setbuf(stdin,NULL);
+	tam++;
 }
 
 int menu(){
@@ -30,19 +30,19 @@ int menu(){
 }
 
 void opcao(struct REGISTRO *dado, int x){
-	struct REGISTRO reg;
-	reg.prox = NULL;
+	struct REGISTRO *reg =(struct REGISTRO *) malloc(sizeof(struct REGISTRO));
+	reg->prox = NULL;
 	switch(x){
 		case 1:
-			criar(&reg);
+			criar(reg);
 			inserirInicio(dado,reg);
 			break;
-		/*case 2:
-			criar(&reg);
-			inserirPos(dado,reg);
-			break;*/
+		//case 2:
+			//criar(&reg);
+			//inserirPos(dado,reg);
+			//break;
 		case 3:
-			criar(&reg);
+			criar(reg);
 			inserirFim(dado,reg);
 			break;
 		case 4:
@@ -61,28 +61,27 @@ int vazia(struct REGISTRO *inicio){ //verifica se a lista está vazia
 		return 0;
 	}
 }
-int inserirInicio(struct REGISTRO *inicio, struct REGISTRO aux){//Inserir no inicio da lista
-	aux.prox = inicio;
-	inicio->prox = &aux;
-	tam++;
-	return 0;
+void inserirInicio(struct REGISTRO *inicio, struct REGISTRO *aux){//Inserir no inicio da lista
+	aux->prox = inicio;
+	inicio = aux;
+	
 }
 
 /*int inserirPos(struct REGISTRO *inicio, struct REGISTRO dados){
 	
 }*/
 
-int inserirFim(struct REGISTRO *inicio, struct REGISTRO dados){//insere um elemento no fim da lista
+int inserirFim(struct REGISTRO *inicio, struct REGISTRO *dados){//insere um elemento no fim da lista
 	struct REGISTRO *aux = malloc(sizeof(struct REGISTRO));
-	dados.prox = NULL;
+	dados->prox = NULL;
 	if(vazia(inicio)){
-		inicio->prox = &dados;
+		inicio->prox = dados;
 	}else{
 		aux->prox = inicio;
 		while(aux->prox != NULL){
 			aux = aux->prox;
 		}
-		aux->prox = &dados;
+		aux->prox = dados;
 	}
 	tam++;
 }
@@ -96,7 +95,20 @@ void exibir(struct REGISTRO *dados){ //exibe todos os registros da lista
 	int i = 0;
 	struct REGISTRO *aux = (struct REGISTRO*)malloc(sizeof(struct REGISTRO));
 	aux = dados;
-	while(1){
+	printf("%d",tam);
+
+	printf("---------------------------\n");
+	printf("Nome[%d]: %s\n",i+1,aux->nome);
+	printf("Idade[%d]: %d\n",i+1,aux->idade);
+
+	aux = aux->prox;
+	 i++;
+
+	printf("---------------------------\n");
+	printf("Nome[%d]: %s\n",i+1,aux->nome);
+	printf("Idade[%d]: %d\n",i+1,aux->idade);
+
+	/*while(1){
 		printf("---------------------------\n");
 		printf("Nome[%d]: %s\n",i+1,aux->nome);
 		printf("Idade[%d]: %d\n",i+1,aux->idade);
@@ -104,8 +116,7 @@ void exibir(struct REGISTRO *dados){ //exibe todos os registros da lista
 			break;
 		}
 		aux = aux->prox;
-		i++;
-	}
+	}*/
 	printf("---------------------------\n");
 }
 
--- tad.h
+++ tad.h
@@ -17,11 +17,11 @@ void esvaziar(struct REGISTRO *inicio);
 
 void opcao(struct REGISTRO *dado, int x);
 
-int inserirInicio(struct REGISTRO *inicio, struct REGISTRO aux);
+void inserirInicio(struct REGISTRO *inicio, struct REGISTRO *aux);
 
 //int inserirPos(struct REGISTRO *inicio, struct REGISTRO dados);
 
-int inserirFim(struct REGISTRO *inicio, struct REGISTRO dados);
+int inserirFim(struct REGISTRO *inicio, struct REGISTRO *dados);
 
 void esvaziar(struct REGISTRO *inicio);
 
